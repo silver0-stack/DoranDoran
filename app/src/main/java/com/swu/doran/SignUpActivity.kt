67c6aab747b8434e5142ac0ecf8fe32f75f41821
silver0-stack <qmfpsp@naver.com>
2021-11-26 00:41:20 +0900

    온보딩 액티비티 연결

---
+++ app/src/main/java/com/swu/doran/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.swu.doran
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+
+    }
+
+    fun signUpComplete(view: android.view.View) {}
+}
