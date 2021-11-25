918088bd5d05028575535ce25ceef4ca54d3f279
silver0-stack <qmfpsp@naver.com>
2021-11-25 11:09:44 +0900

    test commit

--- app/src/main/java/com/swu/doran/MainActivity.kt
+++ app/src/main/java/com/swu/doran/MainActivity.kt
@@ -15,6 +15,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
+//test
 class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SimpleDateFormat")
